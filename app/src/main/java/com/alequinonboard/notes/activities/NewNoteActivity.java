66c21a0104903c4ed444a0e40b8159f8c7e1a59c
J Cox <J Cox>
2017-03-24 17:54:31 +0000

    added number of icons to drawable file. added icons to new notes activity tool bar

--- app/src/main/java/com/alequinonboard/notes/activities/NewNoteActivity.java
+++ app/src/main/java/com/alequinonboard/notes/activities/NewNoteActivity.java
@@ -1,10 +1,13 @@
 package com.alequinonboard.notes.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.alequinonboard.notes.R;
@@ -17,6 +20,36 @@ public class NewNoteActivity extends AppCompatActivity {
         setContentView(R.layout.activity_new_note);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_notes_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        switch(id){
+
+            case R.id.add_icon_action_bar:
+                startActivity(new Intent(this, NewNoteActivity.class));
+                break;
+
+            case R.id.search_icon_action_bar:
+                break;
+
+            case R.id.action_settings:
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 }
